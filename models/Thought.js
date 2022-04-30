9e9eee6b5a6a46963c8db952211dc7c8e45f6851
Mitchel Wachtel <mitchel.wachtel@gmail.com>
2022-04-30 14:40:18 -0400

    created Thought model

---
+++ models/Thought.js
@@ -0,0 +1,37 @@
+const { Schema, Types } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 280,
+      minlength: 1,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual('reactionCount').get(function() {
+  return this.reactions.length;
+})
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
